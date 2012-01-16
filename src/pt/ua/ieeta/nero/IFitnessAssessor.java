89e9d5f9ef2f915a4e3ee9856baf9434e107d8e6
Paulo Gaspar <paulogaspar@ua.pt>
2012-01-16 18:01:56 +0000

    New interface for the fitness assessing class IFitnessAssessor
    Signed-off-by: Paulo Gaspar <paulogaspar@ua.pt>

---
+++ src/pt/ua/ieeta/nero/IFitnessAssessor.java
@@ -0,0 +1,13 @@
+
+package pt.ua.ieeta.nero;
+
+import java.util.List;
+
+/**
+ *
+ * @author Paulo
+ */
+public interface IFitnessAssessor
+{
+    public double getFitness(List<Feature> featureList);
+}
